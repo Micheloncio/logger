dd3db2bd7b812385e8e5bca218fb1f00a940ae18
Micheloncio <escolano_garnica@hotmail.com>
2018-02-23 16:38:54 +0100

    FileLogger-test.js

---
+++ FileLogger-test.js
@@ -0,0 +1,11 @@
+//js para testear que funciona FileLogger
+
+const { FileLogger } = require('.')
+
+const log = new FileLogger('server', 'server.log')
+
+log.debug('this is a debug message')
+log.info('this is a info message')
+log.warn('this is a warn message')
+log.error('this is a error message')
+log.fatal('this is a fatal message')
